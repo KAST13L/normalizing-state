dce591d55c12c8090b48c6ba85eeadabcbd88f4a
KAST13L <o4enbsmeshno@gmail.com>
2023-03-23 14:16:52 +0200

    feat: add new methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {PostsPage} from "./pages/PostsPage";
+import {WeatherComponent} from "weather-widget-test2";
 
 export const App: React.FC = () => {
 
     return (
         <div>
             <PostsPage/>
+            <WeatherComponent/>
         </div>
     );
 };
