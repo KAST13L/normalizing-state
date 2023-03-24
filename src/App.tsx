74ab1dc1e3dcdf4520fc8e856180b08a5468b06e
KAST13L <o4enbsmeshno@gmail.com>
2023-03-24 20:55:38 +0200

    feat: add weather component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import {PostsPage} from "./pages/PostsPage";
 import {WeatherComponent} from "weather-widget-test2";
 
 export const App: React.FC = () => {
-
     return (
         <div>
             <PostsPage/>
