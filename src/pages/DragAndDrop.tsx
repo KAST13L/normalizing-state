77d66dd3c4c816d1f3b3d83d4286194d27e56f9e
KAST13L <o4enbsmeshno@gmail.com>
2023-03-29 21:30:11 +0300

    feat: realization drag and drop

--- src/pages/DragAndDrop.tsx
+++ src/pages/DragAndDrop.tsx
@@ -1,20 +1,71 @@
 import React, {useState} from 'react';
 
+interface CardType {
+    id: number
+    order: number
+    title: string
+}
+
 export const DragAndDrop = () => {
 
-    const [cardList, setCardList] = useState([
-        {id:1, order: 1, title:'Card One'},
-        {id:2, order: 2, title:'Card Two'},
-        {id:3, order: 4, title:'Card Three'},
-        {id:4, order: 3, title:'Card Four'}
+    const [cardList, setCardList] = useState<CardType[]>([
+        {id: 1, order: 1, title: 'Card One'},
+        {id: 2, order: 2, title: 'Card Two'},
+        {id: 3, order: 4, title: 'Card Three'},
+        {id: 4, order: 3, title: 'Card Four'}
     ])
+    const [currentCard, setCurrentCard] = useState<CardType | any>(null)
+
+    function onDragStartHandler(e: React.DragEvent<HTMLDivElement>, card: { id: number; title: string; order: number }) {
+        setCurrentCard(card)
+    }
+
+    function onDragLeaveHandler(e: React.DragEvent<HTMLDivElement>) {
+        e.currentTarget.style.background = 'coral'
+    }
+
+    function onDragEndHandler(e: React.DragEvent<HTMLDivElement>) {
+
+    }
+
+    function onDragOverHandler(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault()
+        e.currentTarget.style.background = 'lightgray'
+    }
+
+    function onDropHandler(e: React.DragEvent<HTMLDivElement>, card: CardType) {
+        e.preventDefault()
+        setCardList(cardList.map( c => {
+            if (c.id === card.id) {
+                return {...c, order: currentCard.order}
+            }
+            if (c.id === currentCard?.id) {
+                return {...c, order: card.order}
+            }
+            return c
+        }))
+        e.currentTarget.style.background = 'coral'
+    }
+
+    function sortCards (a: CardType, b: CardType) {
+        if (a.order > b.order) {
+            return 1
+        } else {
+            return -1
+        }
+    }
 
     return (
         <div className={'app'}>
-            {cardList.map(card => <div
+            {cardList.sort(sortCards).map(card => <div
                 className={'card'}
                 key={card.id}
                 draggable
+                onDragStart={(e) => onDragStartHandler(e, card)}
+                onDragLeave={(e) => onDragLeaveHandler(e)}
+                onDragEnd={(e) => onDragEndHandler(e)}
+                onDragOver={(e) => onDragOverHandler(e)}
+                onDrop={(e) => onDropHandler(e, card)}
             >{card.title}</div>)}
         </div>
     );
